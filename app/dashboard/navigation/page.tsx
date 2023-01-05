c1f8e4ea74f2f41d2b75c81a01870d2f8dc09e6b
Cameron Clough <cameronjclough@gmail.com>
2023-01-04 22:15:18 -0800

    add map

--- app/dashboard/navigation/page.tsx
+++ app/dashboard/navigation/page.tsx
@@ -1,5 +1,35 @@
+'use client';
+
+import { useState } from 'react';
+import Map, { GeolocateControl, Marker } from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiY29tbWFhaSIsImEiOiJjangyYXV0c20wMGU2NDluMWR4amUydGl5In0.6Vb11S6tdX6Arpj6trRE_g';
+const MAP_STYLE = 'mapbox://styles/commaai/clcgvbi4f000q15t6o2s8gys3';
+
 export default function Nav() {
+  const [viewState, setViewState] = useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14
+  });
+
   return (
-    <h1>Navigation</h1>
+    <Map
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
+      initialViewState={{
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14
+      }}
+      style={{ position: 'fixed', left: 0, right: 0, top: 56, bottom: 56 }}
+      mapStyle={MAP_STYLE}
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={-122.4} latitude={37.8} color="red" />
+      <GeolocateControl />
+    </Map>
   );
 }
