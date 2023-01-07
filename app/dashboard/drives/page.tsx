aa51003fc40fbde3a7dd2200a470a453ccdbd859
Cameron Clough <cameronjclough@gmail.com>
2023-01-07 15:36:52 -0800

    example route cards

--- app/dashboard/drives/page.tsx
+++ app/dashboard/drives/page.tsx
@@ -1,7 +1,68 @@
+'use client';
+
+import Image from 'next/image';
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+
+import map from './map_timeline.png';
+
+const routes = [
+  {
+    time: '6:13-8:14',
+    location: 'San Diego, California',
+    distance: '12 mi',
+    duration: '1h 5m',
+    points: 23,
+  },
+  {
+    time: '6:13-8:14',
+    location: 'San Diego, California',
+    distance: '12 mi',
+    duration: '1h 5m',
+    points: 23,
+  },
+  {
+    time: '6:13-8:14',
+    location: 'San Diego, California',
+    distance: '12 mi',
+    duration: '1h 5m',
+    points: 23,
+  },
+];
+
+function RouteCard(route: typeof routes[0]) {
+  return (
+    <Grid xs={12}>
+      <Card onClick={() => null}>
+        <CardActionArea>
+          <CardHeader
+            title={route.location}
+            subheader={route.time}
+          />
+          <Box sx={{ position: 'relative', height: 200 }}>
+            <Image src={map} fill alt="map" />
+          </Box>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
+}
+
 export default function Drives() {
   return (
-    <h1>
-      Drives
-    </h1>
+    <Container sx={{ p: 2, mt: '72px', mb: '56px' }}>
+      <Grid container spacing={2}>
+        {routes.map(RouteCard)}
+      </Grid>
+    </Container>
   );
 }
