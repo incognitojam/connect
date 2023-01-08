94447b16c322e074bdefe0c4236c7e19db1304df
Cameron Clough <cameronjclough@gmail.com>
2023-01-07 18:18:50 -0800

    cleanup

--- app/dashboard/clips/page.tsx
+++ app/dashboard/clips/page.tsx
@@ -44,14 +44,14 @@ function hexToRGB(hex: string, alpha: number): string {
   if (hex.length === 4) {
     hex = '#' + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
   }
-  var r = parseInt(hex.slice(1, 3), 16),
+  const r = parseInt(hex.slice(1, 3), 16),
     g = parseInt(hex.slice(3, 5), 16),
     b = parseInt(hex.slice(5, 7), 16);
 
   if (alpha) {
-    return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
   } else {
-    return "rgb(" + r + ", " + g + ", " + b + ")";
+    return `rgb(${r}, ${g}, ${b})`;
   }
 }
 
