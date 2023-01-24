83e023ed25f866b55f327d1166b81566c9e40dc9
Cameron Clough <cameronjclough@gmail.com>
2023-01-24 14:27:45 -0800

    types

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
