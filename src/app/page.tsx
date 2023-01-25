d046691f15340567ae158f5a2affd126a4c13794
Cameron Clough <cameronjclough@gmail.com>
2023-01-24 19:22:12 -0800

    theme tokens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { Inter } from '@next/font/google'
-import { Switch } from '@headlessui/react'
+import { Switch, Tab } from '@headlessui/react'
 
 import Button from '@/components/Button'
 
@@ -10,31 +10,57 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [enabled, setEnabled] = useState(false)
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const pages = [
+    'Navigation',
+    'Timeline',
+    'Clips',
+  ]
 
   return (
-    <div className="mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-lg">
-      <div className="shrink-0">
-        <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
-      </div>
-      <div>
-        <div className="text-xl font-medium text-black">ChitChat</div>
-        <p className="text-slate-500">You have a new message!</p>
-        <Button>Click me</Button>
-        <Switch
-          checked={enabled}
-          onChange={setEnabled}
-          className={`${
-            enabled ? 'bg-blue-600' : 'bg-gray-200'
-          } relative inline-flex h-6 w-11 items-center rounded-full transition`}
-        >
-          <span className="sr-only">Enable notifications</span>
-          <span
+    <>
+      <div className="mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-lg">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+          <Button>Click me</Button>
+          <Switch
+            checked={enabled}
+            onChange={setEnabled}
             className={`${
-              enabled ? 'translate-x-6' : 'translate-x-1'
-            } inline-block h-4 w-4 rounded-full bg-white transition`}
-          />
-        </Switch>
+              enabled ? 'bg-blue-600' : 'bg-gray-200'
+            } relative inline-flex h-6 w-11 items-center rounded-full transition`}
+          >
+            <span className="sr-only">Enable notifications</span>
+            <span
+              className={`${
+                enabled ? 'translate-x-6' : 'translate-x-1'
+              } inline-block h-4 w-4 rounded-full bg-white transition`}
+            />
+          </Switch>
+        </div>
       </div>
-    </div>
+
+      <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+        <Tab.List className="space-x-2">
+          {pages.map((page, index) => (
+            <Tab
+              key={index}
+              className={({ selected }) =>
+                `py-2 px-4 text-sm font-medium ${
+                  selected ? 'text-blue-600' : 'text-gray-500'
+                } border border-blue-600 rounded-md`
+              }
+            >
+              {page}
+            </Tab>
+          ))}
+        </Tab.List>
+      </Tab.Group>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
+const kebabcase = require('lodash.kebabcase')
+
+const theme = require('./src/theme/theme.json')
+const colours = Object.entries(Object
+  .values(theme.schemes.dark))
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx'],
