1a7ac9aa63457b46665b24d1f9cd7a8cd0afd825
Cameron Clough <cameronjclough@gmail.com>
2023-01-26 20:26:21 -0800

    add @use-gesture and react-spring

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,9 +1,19 @@
 import React, { ReactNode } from 'react'
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
 
 export interface DrawerProps {
   children: ReactNode | ReactNode[]
 }
 
 export default function Drawer({ children }: DrawerProps) {
-  return <div>{children}</div>
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
+
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down })
+  })
+
+  return <animated.div {...bind()} style={{ x, y }}>
+    {children}
+  </animated.div>
 }
