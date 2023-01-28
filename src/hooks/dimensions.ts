a54cc6a27848d0edeed5d6224151b82901958c32
Cameron Clough <cameronjclough@gmail.com>
2023-01-27 22:48:38 -0800

    fix(dimensions): window is only available on the client

--- src/hooks/dimensions.ts
+++ src/hooks/dimensions.ts
@@ -1,6 +1,9 @@
+'use client'
+
 import { useState, useEffect } from 'react'
 
 export function getWindowDimensions() {
+  if (typeof window === 'undefined') return { width: 0, height: 0 }
   const { innerWidth: width, innerHeight: height } = window
   return { width, height }
 }
