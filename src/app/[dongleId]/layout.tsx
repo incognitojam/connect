95b68ff0e07d318485218c72401984d7b350c5ac
Cameron Clough <cameronjclough@gmail.com>
2023-01-27 22:41:11 -0800

    feat(Drawer): covers screen width

--- src/app/[dongleId]/layout.tsx
+++ src/app/[dongleId]/layout.tsx
@@ -4,105 +4,74 @@ import React from 'react'
 import { animated, useSpring } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 
-import AppBar from '@/components/AppBar'
 import NavigationBar, { NavigationBarItem } from '@/components/NavigationBar'
+import useWindowDimensions from '@/hooks/dimensions'
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  // navigation drawer on left is 256px wide
-  // hidden by main content
-  // revealed by swiping
+  const [contentStyles, contentRef] = useSpring({ x: 0 }, [])
+  const [drawerStyles, drawerRef] = useSpring({ x: -16, opacity: 0 }, [])
 
-  const [styles, api] = useSpring({ x: 0 }, [])
-  const { x } = styles
+  const { width } = useWindowDimensions()
 
   const bind = useDrag(
-    ({ last, movement: [mx], memo = x.get(), direction, velocity }) => {
+    ({
+      last,
+      movement: [mx],
+      memo = contentStyles.x.get(),
+      direction,
+      velocity,
+    }) => {
       if (last) {
         const projection = (startVelocity: number) =>
           (startVelocity * 0.998) / (1 - 0.998)
 
         const velocityX = velocity[0] * direction[0]
-        const projectedX = projection(velocityX)
-        console.log({
-          velocityX,
-          projectedX,
-          result: memo + mx + projectedX,
-        })
-
         const projectedEndpoint = memo + mx + projection(velocityX)
         const open = projectedEndpoint > 96
-        api.start({ x: open ? 256 : 0 })
+        contentRef.start({ x: open ? width : 0 })
+        drawerRef.start({ x: open ? 0 : -16, opacity: open ? 1 : 0 })
         return memo
       }
 
-      api.set({ x: Math.min(256, Math.max(0, memo + mx)) })
+      const x = Math.min(width, Math.max(0, memo + mx))
+
+      // translate content
+      contentRef.set({ x })
+
+      // animate drawer
+      const pct = x / width
+      drawerRef.set({
+        x: -16 + 16 * pct,
+        opacity: pct,
+      })
       return memo
     },
   )
 
   return (
     <div className="overflow-x-hidden">
-      <nav>
-        <div
+      <nav className="bg-background">
+        <animated.div
           {...bind()}
-          className="absolute inset-y-0 left-0 w-64 touch-pan-y overflow-y-auto bg-surface-variant text-on-surface-variant"
+          className="absolute inset-y-0 left-0 w-screen touch-pan-y overflow-y-auto bg-surface text-on-surface-variant"
+          style={drawerStyles}
         >
-          <AppBar relative>
-            <h1 className="text-[24px] font-bold">comma connect</h1>
-          </AppBar>
+          <h1 className="text-[24px] font-bold">comma connect</h1>
 
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-          <p>Item 4</p>
-          <p>Item 5</p>
-          <p>Item 6</p>
-
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-          <p>Item 4</p>
-          <p>Item 5</p>
-          <p>Item 6</p>
-
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-          <p>Item 4</p>
-          <p>Item 5</p>
-          <p>Item 6</p>
-
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-          <p>Item 4</p>
-          <p>Item 5</p>
-          <p>Item 6</p>
-
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-          <p>Item 4</p>
-          <p>Item 5</p>
-          <p>Item 6</p>
-
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-          <p>Item 4</p>
-          <p>Item 5</p>
-          <p>Item 6</p>
-        </div>
+          {[...Array(50)].map((_, i) => (
+            <p key={i}>Item {i}</p>
+          ))}
+        </animated.div>
       </nav>
 
       <animated.main
         {...bind()}
         className="relative h-screen flex-1 touch-none overflow-y-auto bg-background"
-        style={styles}
+        style={contentStyles}
       >
         {children}
 
