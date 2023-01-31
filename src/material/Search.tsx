e720a4b960237b3b80435a34c787a4c3b0be9483
Cameron Clough <cameronjclough@gmail.com>
2023-01-30 19:47:05 -0800

    refactor(Search): styles cleanup

--- src/material/Search.tsx
+++ src/material/Search.tsx
@@ -17,13 +17,13 @@ export default function Search({
   return (
     <div
       className={clsx(
-        'elevation-3 flex h-14 min-w-[320px] max-w-[720px] items-center justify-between rounded-full bg-surface px-4 text-on-surface',
+        'elevation-3 flex h-14 min-w-[320px] max-w-[720px] items-center justify-between gap-4 rounded-full bg-surface px-4 text-on-surface',
         className,
       )}
     >
-      {leading && <div className="mr-4">{leading}</div>}
-      <div className="w-full">{children}</div>
-      {trailing && <div className="ml-4">{trailing}</div>}
+      {leading}
+      <span className="w-full">{children}</span>
+      {trailing}
     </div>
   )
 }
